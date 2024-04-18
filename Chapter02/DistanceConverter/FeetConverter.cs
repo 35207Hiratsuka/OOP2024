e6db1a55cc2cd832aaf2783ed51a49576de23113
HiratsukaShinichi <oibc35207@ojs.ac.jp>
2024-04-18 14:48:04 +0900

    静的クラス

--- Chapter02/DistanceConverter/FeetConverter.cs
+++ Chapter02/DistanceConverter/FeetConverter.cs
@@ -8,12 +8,12 @@ namespace DistanceConverter {
     internal class FeetConverter {
 
         //メートルからフィードを求める
-        static double FromMeter(double meter) {
+        public static double FromMeter(double meter) {
             return meter / 0.3048;
         }
 
         //フィードからメートルを求める
-        static double ToMeter(double feet) {
+        public static double ToMeter(double feet) {
             return feet * 0.3048;
         }
     }
--- Chapter02/DistanceConverter/Program.cs
+++ Chapter02/DistanceConverter/Program.cs
@@ -18,10 +18,9 @@ namespace DistanceConverter {
 
 
                 //フィードからメートルへの対応表を出力
-                FeetConverter FC = new FeetConverter();
                 static void PrintFeetToMeterList(int start, int stop) {
                     for(int feet = start; feet <= stop; feet++) {
-                    double meter = FC.ToMeter(feet);
+                    double meter = FeetConverter.ToMeter(feet);
                     Console.WriteLine("{0} ft = {1:0.0000} m",
                         feet, meter);
                     }
@@ -30,7 +29,7 @@ namespace DistanceConverter {
                 //メートルからフィードへの対応表を出力
                 static void PrintMeterToFeetList(int start, int stop) {
                 for(int meter = start; meter <= stop; meter++) {
-                    double feet = FC.FromMeter(meter);
+                    double feet = FeetConverter.FromMeter(meter);
                     Console.WriteLine("{0} m = {1:0.0000} ft",
                         meter, feet);
                 }
