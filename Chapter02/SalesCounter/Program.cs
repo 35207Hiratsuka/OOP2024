90fff0ce564146fa79eb19f5ea0f3822782e8e4e
HiratsukaShinichi <oibc35207@ojs.ac.jp>
2024-04-24 10:53:16 +0900

    売上集計プログラム新規追加

---
+++ Chapter02/SalesCounter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace SalesCounter {
+    internal class Program {
+        static void Main(string[] args) {
+            //フォルダからのファイルからのパスのコピー
+            List<Sale> sales = ReadSales(@"C:\Users\infosys\source\repos\OOP2024\Chapter02\SalesCounter\bin\Debug\data\Sales.csv");
+            
+
+
+            //戻り値のコレクションを一件ずつ出力する
+            foreach(Sale sale in sales) {
+
+                Console.WriteLine(sale.ShopName + " " + sale.ProductCategory + " " + sale.Amount);
+                //("店名：{0} カテゴリー：{1} 売上：{2}",sale.ShopName,sale.ProductCategory,sale.Amount)
+                //
+
+            }
+        }
+
+        //売り上げデータを読み込み、Saleオブジェクトのリストを返す
+        static List<Sale> ReadSales(String filePath) {
+            List<Sale> sales = new List<Sale>();
+            String[] lines = File.ReadAllLines(filePath);
+            foreach(String line in lines) {
+                String[] items = line.Split(',');
+                Sale sale = new Sale {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+            return sales;
+        }
+    }
+}
---
+++ Chapter02/SalesCounter/Sale.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesCounter {
+    internal class Sale {
+        //店舗名
+        public String ShopName { get; set; }
+
+        //商品カテゴリ
+        public String ProductCategory { get; set; }
+
+        //売上高
+        public int Amount { get; set; }
+    }
+}
