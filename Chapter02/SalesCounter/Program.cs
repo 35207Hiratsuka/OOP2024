d186ce494789233af16e3154bb89f120294f48fe
HiratsukaShinichi <oibc35207@ojs.ac.jp>
2024-04-24 14:08:23 +0900

    メソッドの移動

--- Chapter02/SalesCounter/Program.cs
+++ Chapter02/SalesCounter/Program.cs
@@ -6,27 +6,13 @@ using System.Security.Cryptography;
 namespace SalesCounter {
     public class Program {
         static void Main(string[] args) {
-            SalesCounter sales = new SalesCounter(ReadSales(@"data\sales.csv"));
+            SalesCounter sales = new SalesCounter("sales.csv");
             Dictionary<string, int> amountPerStore = sales.GetPerStoreSales();
             foreach(KeyValuePair<string, int> obj in amountPerStore) {
                 Console.WriteLine("{0} {1}", obj.Key, obj.Value);
             }
         }
 
-        //売り上げデータを読み込み、Saleオブジェクトのリストを返す
-        static List<Sale> ReadSales(String filePath) {
-            List<Sale> sales = new List<Sale>();
-            String[] lines = File.ReadAllLines(filePath);
-            foreach(String line in lines) {
-                String[] items = line.Split(',');
-                Sale sale = new Sale {
-                    ShopName = items[0],
-                    ProductCategory = items[1],
-                    Amount = int.Parse(items[2])
-                };
-                sales.Add(sale);
-            }
-            return sales;
-        }
+       
     }
 }
--- Chapter02/SalesCounter/SalesCounter.cs
+++ Chapter02/SalesCounter/SalesCounter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,21 +10,35 @@ namespace SalesCounter {
         private List<Sale> _sales;
 
         //コンストラクタ
-        public SalesCounter(List<Sale> sales) {
-            _sales = sales;
+        public SalesCounter(string filePath) {
+            _sales = ReadSales(filePath);
         }
 
-
+        //売り上げデータを読み込み、Saleオブジェクトのリストを返す
+        static List<Sale> ReadSales(String filePath) {
+            List<Sale> sales = new List<Sale>();
+            String[] lines = File.ReadAllLines(filePath);
+            foreach(String line in lines) {
+                String[] items = line.Split(',');
+                Sale sale = new Sale {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+            return sales;
+        }
 
 
         //店舗別売上を求める
         public Dictionary<string, int> GetPerStoreSales() {
             Dictionary<string, int> dict = new Dictionary<string, int>();
             foreach(Sale sale in _sales) {
-                if(dict.ContainsKey(sale.ShopName))
-                    dict[sale.ShopName] += sale.Amount;
+                if(dict.ContainsKey(sale.ProductCategory))
+                    dict[sale.ProductCategory] += sale.Amount;
                 else
-                    dict[sale.ShopName] = sale.Amount;
+                    dict[sale.ProductCategory] = sale.Amount;
             }
             return dict;
         }
