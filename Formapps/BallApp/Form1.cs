a83143298cea10610ecdcb26e70b59f070d2f8b1
HiratsukaShinichi <oibc35207@ojs.ac.jp>
2024-05-08 15:48:01 +0900

    複数のボールが同時に移動

--- Formapps/BallApp/Form1.cs
+++ Formapps/BallApp/Form1.cs
@@ -1,7 +1,11 @@
 namespace BallApp {
     public partial class Form1 : Form {
-        Obj ball;
-        PictureBox pb;
+        //Obj ball;
+        //PictureBox pb;
+
+        //Listコレクション
+        private List<Obj> balls = new List<Obj>(); //ボールインスタンス格納用
+        private List<PictureBox> pbs = new List<PictureBox>(); //表示用
 
         //コンストラクタ
         public Form1() {
@@ -14,15 +18,23 @@ namespace BallApp {
         }
 
         private void timer1_Tick(object sender, EventArgs e) {
-            ball.Move();
-            pb.Location = new Point((int)ball.PosX, (int)ball.PosY);
+            //ball.Move();
+            //pb.Location = new Point((int)ball.PosX, (int)ball.PosY);
+
+            for(int i = 0; i < balls.Count; i++) {
+                balls[i].Move();
+                pbs[i].Location = new Point((int)balls[i].PosX, (int)balls[i].PosY);
+
+            }
+
         }
-        
+
+
         private void Form1_MouseClick(object sender, MouseEventArgs e) {
 
 
-            pb = new PictureBox();   //画像を表示するコントロール
-            
+            PictureBox pb = new PictureBox();   //画像を表示するコントロール
+            Obj ball = null;
 
             if(e.Button == MouseButtons.Left) {
                 pb.Size = new Size(50, 50);
@@ -30,7 +42,7 @@ namespace BallApp {
 
             } else if(e.Button == MouseButtons.Right) {
                 pb.Size = new Size(25, 25);
-                ball = new tennisBall(e.X - 25, e.Y - 25);
+                ball = new tennisBall(e.X - 12, e.Y - 12);
             }
 
             pb.Image = ball.Image;
@@ -38,6 +50,10 @@ namespace BallApp {
             pb.SizeMode = PictureBoxSizeMode.StretchImage;
             pb.Parent = this;
             timer1.Start();
+
+            balls.Add(ball);
+            pbs.Add(pb);
         }
     }
 }
+
