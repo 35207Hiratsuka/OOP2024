25e7d83da5a35c3e403ce10cd95bbd3a30a8e424
HiratsukaShinichi <oibc35207@ojs.ac.jp>
2024-05-29 15:49:58 +0900

    5章練習問題 5.2まで

---
+++ Chapter05/演習問題/Exercise01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("文字列を入力(1)");
+            var str1 = Console.ReadLine();
+            Console.WriteLine("文字列を入力(2)");
+            var str2 = Console.ReadLine();
+            if(String.Compare(str1, str2, true) == 0) {
+                Console.WriteLine("等しい");
+            } else {
+                Console.WriteLine("等しくない");
+            }
+        }
+    }
+}
---
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("数値を入力");
+            var Snum = Console.ReadLine();
+            int num = int.Parse(Snum);
+            var Knum = num.ToString("#,0");
+            Console.WriteLine(Knum);
+        }
+    }
+}
