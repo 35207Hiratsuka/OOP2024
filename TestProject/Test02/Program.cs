91b1424267b03b2f1e152359e726a20ad26f64a7
HiratsukaShinichi <oibc35207@ojs.ac.jp>
2024-06-18 12:00:56 +0900

    設問２_提出

--- TestProject/Test02/Program.cs
+++ TestProject/Test02/Program.cs
@@ -67,48 +67,52 @@ namespace Test02 {
         //問題１　合計値を表示
         //　　　　出力結果【618】
         private static void Exercise01(List<int> numbers) {
+            var sum = numbers.Sum();
 
-
-
+            Console.WriteLine(sum);
         }
 
         //問題２　偶数の最大値を表示
         //　　　　出力結果【94】
         private static void Exercise02(List<int> numbers) {
-
-
-
+            var gMax = numbers.Where(x => x % 2 == 0)
+                .Max();
+                              
+            Console.WriteLine(gMax);
         }
         //問題３　奇数のみを降順に並べて表示（遅延実行とする）
         //　　　　出力結果【91 87 53 35 31 17】
         private static void Exercise03(List<int> numbers) {
+            var kMin = numbers.Where(x => x % 2 == 0);
+                //.OrderByDescending(numbers);
 
 
-
+            Console.WriteLine(kMin);
         }
 
         //問題４　10以上50以下の数字のみを表示（即時実行でも可とする）
         //　　　　出力結果【12 14 20 40 35 31 17 48】
         private static void Exercise04(List<int> numbers) {
+            var mid = numbers.Where(x => 10 <= x || x >= 50);
 
 
-
+            Console.WriteLine(mid);
         }
 
         //問題５　平均年齢を表示
         //　　　　出力結果【30.2才】
         private static void Exercise05(List<Person> persons) {
-            
+            var ave = persons.Average(x => x.Age);
 
-
+            Console.WriteLine(ave);
         }
 
         //問題６　身長170cm以上の体重平均を表示
         //　　　　出力結果【72.25kg】
         private static void Exercise06(List<Person> persons) {
-
-
-
+            var hi = persons.Where(x => x.Height >= 170);
+            var we = hi.Average(x => x.Weight);
+            Console.WriteLine(we);
         }
 
         //問題７　体重70Kg以下の人を全て表示（名前と体重）
@@ -116,7 +120,11 @@ namespace Test02 {
         //　　　　　　　　　沖田宏一 59kg
         //　　　　　　　　　片山伸介 65kg】
         private static void Exercise07(List<Person> persons) {
-        
+            var wei = persons.Where(x => x.Weight <= 70);
+
+            foreach(Person person in wei) {
+                Console.WriteLine(person.Name + " " + person.Weight);
+            }
         
         
         }
@@ -125,9 +133,9 @@ namespace Test02 {
         //　　　　出力結果【山田隆司
         //　　　　　　　　　片山伸介】
         private static void Exercise08(List<Person> persons) {
+            var yama = persons.Where(y => y.Name.Contains("山"));
 
-
-
+            Console.WriteLine(yama);
         }
     }
 }
