969a97a93e6ec6f0305fa26df9f6121d744752f6
HiratsukaShinichi <oibc35207@ojs.ac.jp>
2024-06-18 11:04:03 +0900

    設問１_Student クラス完成

---
+++ TestProject/Test01/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Test01 {
+    class Student {
+        //学生の名前
+        public String Name { get; set; }
+
+        //科目名
+        public String Subject { get; set; }
+
+        //点数
+        public int Amount { get; set; }
+
+
+
+
+    }
+}
