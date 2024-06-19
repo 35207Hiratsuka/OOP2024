cf5dcd34a3bba9b06c13c699c897da632c7d5f47
HiratsukaShinichi <oibc35207@ojs.ac.jp>
2024-06-19 15:38:23 +0900

    ７章

---
+++ Chapter07/Section04/IAbbreviations.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Section03 {
+    internal interface IAbbreviations {
+        string this[string abbr] { get; }
+
+        int Count {
+            get;
+        }
+
+        void Add(string abbr, string japanese);
+        IEnumerable<KeyValuePair<string, string>> FindAll(string substring);
+        bool Remove(string abb);
+        string ToAbbreviation(string japanese);
+    }
+}
