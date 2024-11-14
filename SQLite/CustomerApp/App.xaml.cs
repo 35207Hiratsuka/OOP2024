3a5da89650d55a0e1fa5627e0b40bb9f46cb3d82
HiratsukaShinichi <oibc35207@ojs.ac.jp>
2024-11-14 11:34:57 +0900

    customerapp1114_1134

---
+++ SQLite/CustomerApp/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CustomerApp {
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application {
+        static String databaseName = "Shop.db";
+        static String folderPass = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        public static String databasePass = System.IO.Path.Combine(folderPass, databaseName);
+    }
+}
